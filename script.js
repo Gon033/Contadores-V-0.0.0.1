4f0d4a5a0ff636c14dd41095d5a9c850769bdcc2
Gon033 <devjorgegonzalez@outlook.com>
2024-05-03 17:52:00 -0600

    JS implementation test
    test JS for buttons

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert('testing funtionality');
+// test functionality 
+
+
+function infoFunction() {
+    alert('no hay informacion de momento')
+}
+
+
+
+
+function contactFunction() {
+
+    const nametxt = document.getElementById('name');
+    const emailtxt = document.getElementById('email');
+
+    if (nametxt.value && emailtxt.value != '' ) {
+        alert(nametxt.value + ' su correo: ' + emailtxt.value + ' fueron enviado exitosamente!')
+    }
+    else {
+        alert('FAvor de llenar los datos de contacto')
+    }
+}
+
